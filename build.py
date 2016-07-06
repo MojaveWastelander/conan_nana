7cfffb38ef75bcf8efb18e2705d085524ad22008
MojaveWastelander <nsdapps@live.com>
2016-07-06 14:41:49 +0300

    Library is working with MSVC 14 and above.

--- build.py
+++ build.py
@@ -3,4 +3,11 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="mojavewastelander")
     builder.add_common_builds(pure_c=False)
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["compiler"] == "Visual Studio":
+            print(settings);
+            if float(str(settings["compiler.version"])) >= 14:
+                 filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
     builder.run()
