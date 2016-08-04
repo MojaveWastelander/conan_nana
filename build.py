b3d96c77a2c840e32fee18506baa19967e2ad018
MojaveWastelander <nsdapps@live.com>
2016-08-04 10:41:15 +0300

    Filtering only msvc lower than 14.0.

--- build.py
+++ build.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     for settings, options in builder.builds:
         if settings["compiler"] == "Visual Studio":
             if float(str(settings["compiler.version"])) >= 14:
-                 filtered_builds.append([settings, options])
+                filtered_builds.append([settings, options])
+        else:
+            filtered_builds.append([settings, options])
     builder.builds = filtered_builds
     builder.run()
